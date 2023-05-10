bb0c107a7f7e539e7a719d8703a4e0339db0a03c
chila <chila@AMBAL>
2023-05-10 20:38:35 +0530

    first commit

---
+++ com.sevenrmartsupermarket/src/test/java/com/sevenrmartsupermarket/base/Base.java
@@ -0,0 +1,25 @@
+package com.sevenrmartsupermarket.base;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Base {
+	public WebDriver driver;
+
+	public void initialise(String browser, String url) {
+		if (browser.equals("chrome")) {
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+		} else if (browser.equals("firefox")) {
+			WebDriverManager.firefoxdriver().setup();
+			driver = new FirefoxDriver();
+		} else if (browser.equals("edge")) {
+			WebDriverManager.edgedriver().setup();
+			driver = new EdgeDriver();
+		}
+	}
+}
